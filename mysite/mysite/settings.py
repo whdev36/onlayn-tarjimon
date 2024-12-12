a357019981b271f08e26f7c988185de3034b14d8
whdev36 <whdev36@gmail.com>
2024-12-12 08:23:14 -0800

    salom dunyo

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tarjimon', # Tarjimon ilovasini qo'shish
 ]
 
 MIDDLEWARE = [
